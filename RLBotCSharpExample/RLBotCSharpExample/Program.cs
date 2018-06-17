23a464e9cd8613e21b44e194f0462c23f68d483f
TheBlocks <exploreblocks@gmail.com>
2018-06-17 23:43:28 +0200

    Finish example bot

--- RLBotCSharpExample/RLBotCSharpExample/Program.cs
+++ RLBotCSharpExample/RLBotCSharpExample/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace RLBotCSharpExample
+﻿using RLBotDotNet;
+
+namespace RLBotCSharpExample
 {
     class Program
     {
         static void Main()
         {
+            // BotManager is a generic which takes in your bot as its T type.
+            BotManager<ExampleBot> botManager = new BotManager<ExampleBot>();
+            // Start the server on port 45031.
+            botManager.Start(45031);
         }
     }
 }
