be6fd5d0ae1702c48287a9ef69e120f1edee7d08
JPBotelho <worldoftanksreviewer@gmail.com>
2018-08-07 15:24:01 +0100

    Added extension methods to flatbuffer datatypes (#5)
    Added ExtensionMethods to autogenerated Vector3 types to convert to System.Numeric ones

---
+++ RLBotCSharpExample/RLBotCSharpExample/DataConversion.cs
@@ -0,0 +1,15 @@
+﻿namespace RLBotCSharpExample
+{
+    public static class DataConversion
+    {
+        public static System.Numerics.Vector3 ToVector3(this rlbot.flat.Vector3 vec)
+        {
+            return new System.Numerics.Vector3(vec.X, vec.Y, vec.Z);
+        }
+
+        public static System.Numerics.Vector2 ToVector2(this rlbot.flat.Vector3 vec)
+        {
+            return new System.Numerics.Vector2(vec.X, vec.Y);
+        }
+    }
+}
