c2444f7f58e868856427adeb9d1dca8c98c19800
Redox <exploreblocks@gmail.com>
2019-09-17 19:03:03 +0200

    Remove try-catch in ExampleBot

--- RLBotCSharpExample/RLBotCSharpExample/ExampleBot.cs
+++ RLBotCSharpExample/RLBotCSharpExample/ExampleBot.cs
@@ -16,37 +16,27 @@ namespace RLBotCSharpExample
             // This controller will contain all the inputs that we want the bot to perform.
             Controller controller = new Controller();
 
-            // Wrap gameTickPacket retrieving in a try-catch so that the bot doesn't crash whenever a value isn't present.
-            // A value may not be present if it was not sent.
-            // These are nullables so trying to get them when they're null will cause errors, therefore we wrap in try-catch.
-            try
-            {
-                // Store the required data from the gameTickPacket.
-                Vector3 ballLocation = gameTickPacket.Ball.Value.Physics.Value.Location.Value;
-                Vector3 carLocation = gameTickPacket.Players(this.index).Value.Physics.Value.Location.Value;
-                Rotator carRotation = gameTickPacket.Players(this.index).Value.Physics.Value.Rotation.Value;
+            // Store the required data from the gameTickPacket.
+            // The GameTickPacket's attributes are nullables, so you must use .Value.
+            // It is recommended to create your own internal data structure to avoid the displeasing .Value syntax.
+            Vector3 ballLocation = gameTickPacket.Ball.Value.Physics.Value.Location.Value;
+            Vector3 carLocation = gameTickPacket.Players(this.index).Value.Physics.Value.Location.Value;
+            Rotator carRotation = gameTickPacket.Players(this.index).Value.Physics.Value.Rotation.Value;
 
-                // Calculate to get the angle from the front of the bot's car to the ball.
-                double botToTargetAngle = Math.Atan2(ballLocation.Y - carLocation.Y, ballLocation.X - carLocation.X);
-                double botFrontToTargetAngle = botToTargetAngle - carRotation.Yaw;
-                // Correct the angle
-                if (botFrontToTargetAngle < -Math.PI)
-                    botFrontToTargetAngle += 2 * Math.PI;
-                if (botFrontToTargetAngle > Math.PI)
-                    botFrontToTargetAngle -= 2 * Math.PI;
+            // Calculate to get the angle from the front of the bot's car to the ball.
+            double botToTargetAngle = Math.Atan2(ballLocation.Y - carLocation.Y, ballLocation.X - carLocation.X);
+            double botFrontToTargetAngle = botToTargetAngle - carRotation.Yaw;
+            // Correct the angle
+            if (botFrontToTargetAngle < -Math.PI)
+                botFrontToTargetAngle += 2 * Math.PI;
+            if (botFrontToTargetAngle > Math.PI)
+                botFrontToTargetAngle -= 2 * Math.PI;
 
-                // Decide which way to steer in order to get to the ball.
-                if (botFrontToTargetAngle > 0)
-                    controller.Steer = 1;
-                else
-                    controller.Steer = -1;
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                Console.WriteLine(e.StackTrace);
-            }
+            // Decide which way to steer in order to get to the ball.
+            if (botFrontToTargetAngle > 0)
+                controller.Steer = 1;
+            else
+                controller.Steer = -1;
 
             // Set the throttle to 1 so the bot can move.
             controller.Throttle = 1;
