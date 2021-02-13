94da44badf6c3c24556f694de172c5f8ce0fb55f
Tyler <tarehart@gmail.com>
2021-02-13 11:59:55 -0800

    Configuring auto-run. (#15)
    Also using command line arguments to get the port instead of cfg file.

--- CSharpBot/Bot/Program.cs
+++ CSharpBot/Bot/Program.cs
@@ -1,15 +1,25 @@
-﻿using System.IO;
+﻿using System;
 
 namespace Bot
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            // Read the port from port.cfg.
-            const string file = "port.cfg";
-            string text = File.ReadAllLines(file)[0];
-            int port = int.Parse(text);
+            int port;
+            try
+            {
+                // Read the port from the first argument
+                port = int.Parse(args[0]);
+            }
+            catch (Exception e)
+            {
+                // If you're trying to run in a debugger, look up the port number used in PythonAgent.py
+                // Right-click the project, choose Properties, go to the Debugging section -- there is a box for "Command Arguments", enter the port there.
+                // Example: 45031
+                Console.WriteLine("Could not get port from arguments to CSharp bot!");
+                throw e;
+            }
 
             RLBotDotNet.BotManager<Bot> botManager = new RLBotDotNet.BotManager<Bot>(0);
             // Start the server on the port given in the port.cfg file.
--- PythonAgent/PythonAgent.py
+++ PythonAgent/PythonAgent.py
@@ -1,9 +1,18 @@
-import os
+from rlbot.agents.base_agent import BOT_CONFIG_AGENT_HEADER
+from rlbot.agents.executable_with_socket_agent import ExecutableWithSocketAgent
+from rlbot.parsing.custom_config import ConfigHeader, ConfigObject
 
-from rlbot.agents.base_dotnet_agent import BaseDotNetAgent
 
+class DotNetBot(ExecutableWithSocketAgent):
+    def get_port(self) -> int:
+        return 45031  # Should match port.cfg in the C# area
 
-class DotNetBot(BaseDotNetAgent):
-    def get_port_file_path(self):
-        # Look for a port.cfg file in the same directory as THIS python file.
-        return os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), 'port.cfg'))
+    def load_config(self, config_header: ConfigHeader):
+        self.executable_path = config_header.getpath('executable_path')
+        self.logger.info("Executable is configured as {}".format(self.executable_path))
+
+    @staticmethod
+    def create_agent_configurations(config: ConfigObject):
+        params = config.get_header(BOT_CONFIG_AGENT_HEADER)
+        params.add_value('executable_path', str, default=None,
+                         description='Relative path to the bot executable.')
