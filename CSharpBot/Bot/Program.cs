65b52ce4e9a15e3a8a3d87ed7fd40d8463b4d942
Tangil <21197843+TheBlocks@users.noreply.github.com>
2021-03-09 17:07:07 +0100

    Update error message when port is not received (#16)

--- CSharpBot/Bot/Program.cs
+++ CSharpBot/Bot/Program.cs
@@ -14,7 +14,9 @@ namespace Bot
             }
             catch (Exception e)
             {
-                // If you're trying to run in a debugger, look up the port number used in PythonAgent.py in get_port()
+                // IDE instructions to add the port argument:
+                // If you're trying to run in an IDE and have auto-run disabled (e.g. to use the debugger), look up the port
+                // number used in PythonAgent.py in get_port()
 
                 // If you're using Visual Studio:
                 // Right-click the project, choose Properties, go to the Debug section -- there is a box for
@@ -24,9 +26,23 @@ namespace Bot
                 // Click the configuration dropdown to the left of the Run and Debug buttons (top right of the window),
                 // choose Edit Configurations -- there is a box for "Program arguments", enter the port there.
 
-                // Example: 45031
-                Console.WriteLine("Could not get port from arguments to CSharp bot!");
-                throw e;
+                // Example of a port: 45031
+
+
+                var currentColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(
+                    "Could not get port from arguments to C# bot!\n" +
+                    "If you're reading this message, it means that the C# bot did not receive a valid port in the command line " +
+                    "arguments.\n" +
+                    "If you have configured auto-run, the port should be given to the bot automatically. Otherwise, you'll " +
+                    "need to run the bot with the port every time (e.g. Bot.exe 45031). Note that this port should match the " +
+                    "one in PythonAgent/PythonAgent.py.\n" +
+                    "If you're trying to run the bot without auto-run in an IDE, see this source file " +
+                    "(CSharpBot/Bot/Program.cs) for IDE instructions."
+                );
+                Console.ForegroundColor = currentColor;
+                throw;
             }
 
             RLBotDotNet.BotManager<Bot> botManager = new RLBotDotNet.BotManager<Bot>(0);
